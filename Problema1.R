25348e7e775a9863676f58d0a27d704b62edb1d2
roger.planas.misas <roger.planas.misas@auleseebe.upc.edu>
2025-02-13 13:01:29 +0100

    dia 1

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,28 @@
 #Codigo para problema 1
 
-plot(1:5)
+dim(mtcars)
+
+mis_dades <-mtcars
+
+mis_dades
+
+mean(mis_dades$qsec)#promig
+
+x <- mis_dades$qsec
+sum(x)/length(x) #promig
+
+x <- mis_dades$drat
+
+sort(x)
+median(x)
+quantile(x,0.25)
+quantile(x,0.75)
+
+quantile(mis_dades$mpg,0.18)
+
+quantile(mis_dades$mpg,0.75)-quantile(mis_dades$mpg,0.25)#IQR = q de 0,75 - q de 0,25
+IQR(mis_dades$mpg)
+
+boxplot(mis_dades$mpg)
+IQR(mis_dades$cyl)
+sd(mis_dades$cyl) #desviacio corregida
